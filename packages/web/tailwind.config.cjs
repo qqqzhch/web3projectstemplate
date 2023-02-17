3e2af33d8e4ad2c54bdf375ff58e2a2f17305b1a
zhangchao <544262408@qq.com>
2023-02-17 22:05:43 +0800

    fix: 🐛 config tailwind
    BREAKING CHANGE: 🧨 config tailwind

--- packages/web/tailwind.config.cjs
+++ packages/web/tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,tsx,jsx}", "../../packages/ui-components/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
+    container: {
+      
+      screens: {
+        
+        "2xl": "1905px",
+      },
+    }
     
   },
   plugins: [],
